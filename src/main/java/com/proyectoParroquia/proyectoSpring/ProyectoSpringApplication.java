302d2b40d3fcd17c45971c8ac5b58ceae3587bb8
Carmen123456 <adpirela@misena.edu.co>
2021-06-02 15:30:55 -0500

    primer commit

---
+++ src/main/java/com/proyectoParroquia/proyectoSpring/ProyectoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoParroquia.proyectoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringApplication.class, args);
+	}
+
+}
